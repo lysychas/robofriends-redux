6e26eeb7ea11908160a60c3d17a945d6bbf7dd22
Lukas Lysychas <sayshi108@gmail.com>
2020-03-08 23:02:13 +0200

    class change

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import React from "react";
 const Card = ({ name, email, id }) => {
   //   const { name, email, id } = props; // destructuring in params is COOL
   return (
-    <div className="tc grow bg-light-purple br3 pa3 ma2 dib bw2 shadow-5">
+    <div className="tc bg-light-purple dib br3 pa3 ma2 grow bw2 shadow-5">
       <img src={`https://robohash.org/${id}?size=200x200`} alt="Robot Jane Doe" />
       <div>
         <h2>{name}</h2>
