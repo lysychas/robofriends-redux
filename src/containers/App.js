73da0cfa4e5a909e9bdec8d14689a3f971fd3a32
Lukas Lysychas <sayshi108@gmail.com>
2020-03-09 00:39:53 +0200

    cennecting react app to redux

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 // import { robots } from "./robots";
@@ -6,6 +7,19 @@ import Scroll from "../components/Scroll";
 import ErrorBoundary from "../components/ErrorBoundary";
 import "./App.css";
 
+import { setSearchField } from "../actions";
+
+const mapStateToProps = state => {
+  return {
+    searchField: state.searchField
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onSearchChange: event => dispatch(setSearchField(event.target.value))
+  };
+};
 // const state = {
 //   robots: robots,
 //   searcField: ''
@@ -15,35 +29,38 @@ class App extends Component {
   constructor(params) {
     super();
     this.state = {
-      robots: [],
-      searchfield: ""
+      robots: []
+      // searchfield: ""
     };
   }
 
   componentDidMount() {
+    // console.log(this.props.store.getState())
     fetch("https://jsonplaceholder.typicode.com/users")
       .then(response => response.json())
       .then(users => this.setState({ robots: users }));
     // this.setState({ robots: robots });
   }
 
-  onSearchChange = event => {
-    this.setState({ searchfield: event.target.value });
-  };
+  // onSearchChange = event => {
+  //   this.setState({ searchfield: event.target.value });
+  // };
 
   render() {
-    const { robots, searchfield } = this.state;
+    // const { robots, searchfield } = this.state;
+    const { robots } = this.state;
+    const { searchField, onSearchChange } = this.props;
     const filteredRobots = robots.filter(robot => {
       return robot.name
         .toLocaleLowerCase()
-        .includes(searchfield.toLocaleLowerCase());
+        .includes(searchField.toLocaleLowerCase());
     });
     return !robots.length ? ( //by default js evaluates 0 to false, set ! for true
       <h1 className="tc ttl">Loading</h1>
     ) : (
       <div className="tc">
         <h1 className="f1 ttl">RoboFriends</h1> {/*ttl = to lowercase */}
-        <SearchBox searchChange={this.onSearchChange} />
+        <SearchBox searchChange={onSearchChange} />
         <Scroll>
           <ErrorBoundary>
             <CardList robots={filteredRobots} />
@@ -54,4 +71,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App); // higher order function/component
