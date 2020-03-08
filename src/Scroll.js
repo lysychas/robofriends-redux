386fa799d5dd379d26733cb918b6a859c10731e9
Lukas Lysychas <sayshi108@gmail.com>
2020-03-08 03:26:28 +0200

    adding sticky search header

---
+++ src/Scroll.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+//props
+//state
+//children
+
+const Scroll = props => {
+  console.log(props);
+  return (
+    <div
+      style={{
+        overflowY: "scroll",
+        border: "1px solid black",
+        height: "80vh"
+      }}
+    >
+      {props.children}
+    </div>
+  );
+};
+export default Scroll;
