331a12c4ed5f1bb6a07e742c8da2ebcdbcbdb5c6
Lukas Lysychas <sayshi108@gmail.com>
2020-03-09 02:46:38 +0200

    adding redux thunk

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,74 +1,62 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { setSearchField, requestRobots } from "../actions";
+
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
-// import { robots } from "./robots";
 import Scroll from "../components/Scroll";
 import ErrorBoundary from "../components/ErrorBoundary";
+
 import "./App.css";
 
-import { setSearchField } from "../actions";
-
+// parameter state comes from index.js provider store state(rootReducers)
 const mapStateToProps = state => {
   return {
-    searchField: state.searchField
+    searchField: state.searchRobots.searchField,
+    robots: state.requestRobots.robots,
+    isPending: state.requestRobots.isPending,
+    error: state.requestRobots.error
   };
 };
 
+// dispatch the DOM changes to call an action. note mapStateToProps returns object, mapDispatchToProps returns function
+// the function returns an object then uses connect to change the data from redecers.
 const mapDispatchToProps = dispatch => {
   return {
-    onSearchChange: event => dispatch(setSearchField(event.target.value))
+    onSearchChange: event => dispatch(setSearchField(event.target.value)),
+    onRequestRobots: () => dispatch(requestRobots())
   };
 };
 
-// const state = {
-//   robots: robots,
-//   searcField: ''
-// };
-
 class App extends Component {
-  constructor(params) {
-    super();
-    this.state = {
-      robots: []
-      // searchfield: ""
-    };
-  }
-
   componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then(response => response.json())
-      .then(users => this.setState({ robots: users }));
-    // this.setState({ robots: robots });
+    this.props.onRequestRobots();
   }
-
-  // onSearchChange = event => {
-  //   this.setState({ searchfield: event.target.value });
-  // };
 
   render() {
-    // const { robots, searchfield } = this.state;
-    const { robots } = this.state;
-    const { searchField, onSearchChange } = this.props;
+    const { searchField, onSearchChange, robots, isPending } = this.props;
     const filteredRobots = robots.filter(robot => {
       return robot.name
         .toLocaleLowerCase()
         .includes(searchField.toLocaleLowerCase());
     });
-    return !robots.length ? ( //by default js evaluates 0 to false, set ! for true
-      <h1 className="tc ttl">Loading</h1>
-    ) : (
+    return (
       <div className="tc">
         <h1 className="f1 ttl">RoboFriends</h1> {/*ttl = to lowercase */}
         <SearchBox searchChange={onSearchChange} />
         <Scroll>
-          <ErrorBoundary>
-            <CardList robots={filteredRobots} />
-          </ErrorBoundary>
+          {isPending ? ( //by default js evaluates 0 to false, set ! for true
+            <h1 className="tc ttl">Loading</h1>
+          ) : (
+            <ErrorBoundary>
+              <CardList robots={filteredRobots} />
+            </ErrorBoundary>
+          )}
         </Scroll>
       </div>
     );
   }
 }
 
+// action done from mapDispatchToProps will channge state from mapStateToProps
 export default connect(mapStateToProps, mapDispatchToProps)(App); // higher order function/component
--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Provider, connect } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, combineReducers } from "redux";
+import { Provider } from "react-redux";
 import { createLogger } from "redux-logger";
-import "./index.css";
+import thunkMiddleware from "redux-thunk";
+import "tachyons";
+
 import App from "./containers/App"; // the father of all our children
 import * as serviceWorker from "./serviceWorker";
-import "tachyons";
-import { searchRobots } from "./reducers";
+import { searchRobots, requestRobots } from "./reducers";
+
+import "./index.css";
 
 const logger = createLogger();
-const store = createStore(searchRobots, applyMiddleware(logger)); // source of all truth
+
+const rootReducer = combineReducers({ searchRobots, requestRobots });
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunkMiddleware, logger) // ordered, first thunk, then logger
+); // source of all truth
 
 ReactDOM.render(
   <Provider store={store}>
